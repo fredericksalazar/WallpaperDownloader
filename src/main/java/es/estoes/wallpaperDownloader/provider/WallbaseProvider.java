de81af833fd9a7623d793039c6e93e08bd7415e2
Eloy García Almadén <eloy.garcia.pca@gmail.com>
2014-08-20 13:08:13 +0200

    Now, if a JPG format wallpaper is not succesffully stored, then the application tryes PNG format

--- src/main/java/es/estoes/wallpaperDownloader/provider/WallbaseProvider.java
+++ src/main/java/es/estoes/wallpaperDownloader/provider/WallbaseProvider.java
@@ -6,6 +6,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 
@@ -60,46 +61,44 @@ public class WallbaseProvider extends Provider {
 			LOG.info("Downloading wallpaper with keyword -> " + activeKeyword);
 			// 1.- Getting HTML document
 			Document doc = Jsoup.connect(completeURL).get();
-			// 2.- Getting all thumbnails. They are distinguished because they are 'lazy' classed img elements
+			// 2.- Getting all thumbnails. They are identified because they have 'lazy' classed img elements
 			Elements thumbnails = doc.select("img.lazy");
 			// 3.- Getting a wallpaper which is not already stored in the filesystem
 			for (Element thumbnail : thumbnails) {
 				String thumbnailURL = thumbnail.attr("data-original");
-				// Replacing thumb by wallpaper
+				// Replacing word 'thumb' by word 'wallpaper'
 				String wallpaperURL = thumbnailURL.replace("thumb", "wallpaper");
 				int index = wallpaperURL.lastIndexOf(WDUtilities.URL_SLASH);
-				// Obtaining the wallpaper name (string after the last slash)
+				// Obtaining wallpaper's name (string after the last slash)
 				String wallpaperName = wallpaperURL.substring(index + 1);
 				File wallpaper = new File(WDUtilities.getDownloadsPath() + File.separator + wallpaperName);
 				if (!wallpaper.exists()) {
 					// Storing the image. It is necessary to download the remote file
-					URL url = new URL(wallpaperURL);
-					URLConnection uc = url.openConnection();
-					int contentLength = uc.getContentLength();
-					InputStream input = uc.getInputStream();
-					BufferedInputStream bufIn = new BufferedInputStream(input);
-					OutputStream out = new FileOutputStream(wallpaper);
-					byte[] data = new byte[contentLength];
-					int bytesRead = 0;
-					int offset = 0;
-					while (offset < contentLength) {
-					      bytesRead = bufIn.read(data, offset, data.length - offset);
-					      if (bytesRead == -1)
-					        break;
-					      offset += bytesRead;		
+					// First try: JPG format will be used
+					boolean isWallpaperSuccessfullyStored = storeRemoteFile(wallpaper, wallpaperURL);
+					if (!isWallpaperSuccessfullyStored) {
+						// Second try: PNG format will be used
+						wallpaperURL = wallpaperURL.replace("jpg", "png");
+						LOG.info("JPG format wasn't found. Trying PNG format...");
+						wallpaperName = wallpaperName.replace("jpg", "png");
+						wallpaper = new File(WDUtilities.getDownloadsPath() + File.separator + wallpaperName);
+						if (!wallpaper.exists()) {
+							isWallpaperSuccessfullyStored = storeRemoteFile(wallpaper, wallpaperURL);
+							if (!isWallpaperSuccessfullyStored) {
+								LOG.info("Error trying to store wallpaper " + wallpaperURL + ". Skipping...");							
+							} else {
+								LOG.info("Wallpaper " + wallpaper.getName() + " successfully stored");
+								// Exit the process because one wallpaper was downloaded successfully
+								break;
+							}							
+						} else {
+							LOG.info("This wallpaper is already stored. Skipping...");
+						}
+					} else {
+						LOG.info("Wallpaper " + wallpaper.getName() + " successfully stored");
+						// Exit the process because one wallpaper was downloaded successfully
+						break;
 					}
-					bufIn.close();
-				    if (offset != contentLength) {
-					    out.flush();
-					    out.close();
-				    	throw new ProviderException("Only read " + offset + " bytes; Expected " + contentLength + " bytes");
-				    }
-				    out.write(data);
-				    out.flush();
-				    out.close();				      
-					LOG.info("Wallpaper " + wallpaperName + " successfully stored");
-					// Exit the process because one wallpaper was downloaded successfully
-					break;
 				} else {
 					LOG.info("This wallpaper is already stored. Skipping...");
 				}
@@ -111,6 +110,55 @@ public class WallbaseProvider extends Provider {
 		}
 	}
 		
+	private boolean storeRemoteFile(File wallpaper, String wallpaperURL) {
+		URL url;
+		BufferedInputStream bufIn = null;
+		OutputStream out = null;
+		try {
+			url = new URL(wallpaperURL);
+		} catch (MalformedURLException e) {
+			LOG.error("Malformed URL. Error: " + e.getMessage());
+			return false;
+		}
+		URLConnection uc;
+		try {
+			uc = url.openConnection();
+			int contentLength = uc.getContentLength();
+			InputStream input = uc.getInputStream();
+			bufIn = new BufferedInputStream(input);
+			out = new FileOutputStream(wallpaper);
+			byte[] data = new byte[contentLength];
+			int bytesRead = 0;
+			int offset = 0;
+			while (offset < contentLength) {
+			      bytesRead = bufIn.read(data, offset, data.length - offset);
+			      if (bytesRead == -1)
+			        break;
+			      offset += bytesRead;		
+			}
+			bufIn.close();
+		    if (offset != contentLength) {
+		    	LOG.error("Only read " + offset + " bytes; Expected " + contentLength + " bytes");
+		    	return false;
+		    }
+		    out.write(data);
+		    return true;
+		} catch (IOException e) {
+	    	LOG.error("IOException. Error: " + e.getMessage());
+	    	return false;
+		} finally {
+			if (out!=null) {
+			    try {
+					out.flush();
+				    out.close();				
+				} catch (IOException e) {
+			    	LOG.error("IOException. Error: " + e.getMessage());
+			    	return false;
+				}
+			}
+		}
+	}
+
 	private String composeCompleteURL() {
 		LOG.info(baseURL + "search" + WDUtilities.QM + "q" + WDUtilities.EQUAL + 
 				   activeKeyword + WDUtilities.AND + "res" + WDUtilities.EQUAL + resolution + WDUtilities.AND + "thpp" + WDUtilities.EQUAL + "60" + 
