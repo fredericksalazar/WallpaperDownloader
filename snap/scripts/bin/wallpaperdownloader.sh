a25e057669121815ffba0abfb85ab25702b7f9cd
Eloy García Almadén <eloy.garcia.pca@gmail.com>
2016-09-19 23:10:26 +0200

    Removing exports from init script. I think they are included within snapcraft-desktop-helpers

--- snap/scripts/bin/wallpaperdownloader.sh
+++ snap/scripts/bin/wallpaperdownloader.sh
@@ -2,9 +2,9 @@
 # Only for packaging!
 # Script for snap packaging wallpaperdownloader application. It is not related to the code itself
 # Not good, needed for fontconfig
-export XDG_DATA_HOME=$SNAP/usr/share
+# export XDG_DATA_HOME=$SNAP/usr/share
 # Font Config
-export FONTCONFIG_PATH=$SNAP/etc/fonts/config.d
-export FONTCONFIG_FILE=$SNAP/etc/fonts/fonts.conf
+# export FONTCONFIG_PATH=$SNAP/etc/fonts/config.d
+# export FONTCONFIG_FILE=$SNAP/etc/fonts/fonts.conf
 export HOME=$SNAP_USER_DATA
 desktop-launch java -Xmx512m -Xms256m -jar -Duser.home=$SNAP_USER_DATA $SNAP/jar/wallpaperdownloader.jar
