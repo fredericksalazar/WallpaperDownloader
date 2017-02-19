46e99b4c58d63728a2e68524fcd562ab1d5364b4
Eloy García Almadén <eloy.garcia.pca@gmail.com>
2017-02-19 15:28:26 +0100

    Adding all files needed to create a package for Arch Linux

---
+++ aur/README.txt
@@ -0,0 +1,1 @@
+This directory contains all the files needed to package WallpaperDownloader application for Arch Linux
---
+++ aur/wallpaperdownloader.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# Memory usage is limited to 256 MBytes of RAM
+java -Xmx256m -Xms128m -jar /opt/wallpaperdownloader/jar/wallpaperdownloader.jar
