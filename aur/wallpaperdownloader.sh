a99d35ad3f47670bf4275862ab37234a4e1b26e4
Eloy García Almadén <eloy.garcia.pca@gmail.com>
2017-02-20 09:05:22 +0100

    Updating arch linux packaging files

--- aur/wallpaperdownloader.sh
+++ aur/wallpaperdownloader.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # Memory usage is limited to 256 MBytes of RAM
-java -Xmx256m -Xms128m -jar /opt/wallpaperdownloader/jar/wallpaperdownloader.jar
+exec /usr/bin/java -Xmx256m -Xms128m -jar '/opt/wallpaperdownloader/jar/wallpaperdownloader.jar' "$@"
