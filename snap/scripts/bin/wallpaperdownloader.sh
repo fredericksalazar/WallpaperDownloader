46dd0c79b6606cf7992bca9931ad8bb914a917c5
Eloy García Almadén <eloy.garcia.pca@gmail.com>
2018-02-16 14:01:56 +0100

    Issue #30. The workaround with the old variables didn't work

--- snap/scripts/bin/wallpaperdownloader.sh
+++ snap/scripts/bin/wallpaperdownloader.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 # Only for packaging!
 # Script for snap packaging wallpaperdownloader application. It is not related to the code itself
-# Not good, needed for fontconfig
-export XDG_DATA_HOME=$SNAP/usr/share
-# Font Config
-export FONTCONFIG_PATH=$SNAP/etc/fonts/config.d
-export FONTCONFIG_FILE=$SNAP/etc/fonts/fonts.conf
+# Old exports are not needed anymore because of desktop/gtk3 part use within snapcraft.xml configuration file
 export HOME=$SNAP_USER_DATA
 # Memory usage is limited to 256 MBytes of RAM
 # -Dsun.java2d.xrender=f to avoid RenderBadPicture exception and JVM crashes when previewing wallpapers
