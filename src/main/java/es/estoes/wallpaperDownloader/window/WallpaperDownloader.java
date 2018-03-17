5577550da902fa59a4858bca4e5b65ae0c536af4
Eloy García Almadén <eloy.garcia.pca@gmail.com>
2018-03-17 08:51:20 +0100

    Removing unused library

--- src/main/java/es/estoes/wallpaperDownloader/window/WallpaperDownloader.java
+++ src/main/java/es/estoes/wallpaperDownloader/window/WallpaperDownloader.java
@@ -68,7 +68,6 @@ import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.StringSelection;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
 import java.awt.event.WindowEvent;
